2b3783512d494ee9f534e0e9e7d31d1d4646b3dd
tongclement <clement.yc.tong@gmail.com>
2024-10-12 16:32:03 +0100

    End of progress of Day 3 16:30pm (2:30-4:30pm)

---
+++ heli_programs/v0.5_motortest.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from cued_ia_lego import *
+
+# Try to find connected brick
+try:
+    brick = NXTBrick()
+except Exception:
+    exit()
+
+# Set up rotor motor
+motor_rotor = Motor(brick, PORT_A, power=100, speedreg=False, smoothstart=True, brake=False)
+motor_rotor.reset_position()
+
+# Set up pitch motor position
+motor_pitch = Motor(brick, PORT_B, power=40, speedreg=True, smoothstart=True, brake=True)
+motor_pitch.reset_position()
+target_pitch_motor_absolute_angle = -400
+motor_pitch.turn_to(target_pitch_motor_absolute_angle)
+
+motor_rotor.run()
+motor_armed = True
+
+# Create empty lists for the readings and the times they were recorded
+times = []
+position_readings = []
+
+# Display a message
+print('Starting recording')
+
+# Interrupt any currently active pitch adjustment
+motor_pitch.brake()
+
+# Start time
+t_start = time.perf_counter()
+# Stop time
+t_brake=30
+t_stop = t_start + 33
+
+while time.perf_counter() < t_stop:
+    position_readings.append(motor_rotor.get_position())
+    times.append(time.perf_counter() - t_start)
+    if motor_armed and time.perf_counter() > (t_brake):
+        motor_rotor.idle()
+        motor_armed = False
+
+    current_time_elapsed = time.perf_counter() - t_start
+    motor_rotor.run(power=int(current_time_elapsed / 5) * 20)
+    print(f"Time Elapse {current_time_elapsed}, Current Power Setting {int(current_time_elapsed / 5) * 20}")
+
+
+
+# Turn off the motor
+motor_rotor.idle()
+
+# Calculate rotation speed
+d_positions = np.diff(position_readings)
+d_times = np.diff(times)
+motor_speed = np.zeros(len(d_times) + 1)  # Initialize motor speed array
+motor_speed[1:] = d_positions / d_times  # Populate with speed values
+
+# Display a message
+print('Stopping recording')
+
+# Plot the results
+plt.plot(times, position_readings, label='motor position')
+plt.plot(times, motor_speed, label='motor speed')
+plt.legend()
+plt.grid(True)
+plt.xlabel('time (s)')
+plt.ylabel('motor position/speed')
+
+# Display some information about the results
+num_readings = len(position_readings)
+print(f'The number of readings was: {num_readings}')
+print(f'The recording rate (samples per second) was: {num_readings / 5:.3f}')
+
+print('Close the plot to finish')
+plt.show()
+
+# Export to CSV
+import pandas as pd
+
+data = {
+    'Time (s)': times,
+    'Motor Position': position_readings,
+    'Motor Speed': motor_speed
+}
+
+df = pd.DataFrame(data)
+df.to_csv('motor_test_data.csv', index=False)
+print('Data exported to motor_test_data.csv')
--- heli_programs/v0_motortest.py
+++ heli_programs/v0_motortest.py
@@ -12,7 +12,8 @@ except Exception:
     exit()
 
 # Set up rotor motor
-motor_rotor = Motor(brick, PORT_A, power=100, speedreg=True, smoothstart=True, brake=False)
+#motor_rotor = Motor(brick, PORT_A, power=100, speedreg=True, smoothstart=True, brake=False) #Speedreg is normally True
+motor_rotor = Motor(brick, PORT_A, power=100, speedreg=False, smoothstart=True, brake=False) #Speedreg is normally True
 motor_rotor.reset_position()
 
 # Set up pitch motor position
@@ -20,11 +21,11 @@ motor_pitch = Motor(brick, PORT_B, power=40, speedreg=True,
                     smoothstart=True, brake=True) #Note: If motor needs to be inverted, set the power to negative
 motor_pitch.reset_position()
 pitch_abs_pos = 0
-target_angle=10
-#gear ratio: 16:24
-motor_pitch.turn(-400)
+target_pitch_motor_absolute_angle=-00
+#gear ratio: 16:24 - but irrelevant as you also need to calculate the 2 bar lift if you want to do it mathematically
+motor_pitch.turn_to(target_pitch_motor_absolute_angle) # absolute degrees of motor rotation - call this line of code anywhere anytime to change the pitch
 
-time.sleep(3)
+#time.sleep(3)
 
 motor_rotor.run()
 motor_armed = True #set to False for emergency stop, originally named motor_running
@@ -44,21 +45,28 @@ motor_pitch.brake()
 # program setting that configures how long the helicopter running
 # Start time
 t_start = time.perf_counter()
+
 # Time to apply the brake
-t_brake = t_start + 20
+t_brake = t_start + 42
 # Stop time
-t_stop = t_start + 22
+t_stop = t_start + 45
+
+
 while time.perf_counter() < t_stop:
     # Append lists with current reading and time
     position_readings.append(motor_rotor.get_position())
     times.append(time.perf_counter() - t_start)
 
+    current_time_elapsed = time.perf_counter()-t_start
+    motor_rotor.run(power=int(current_time_elapsed/5)*20)
+    print(f"Time Elapse {current_time_elapsed}, Current Power Setting {int(current_time_elapsed/7)*20}")
 
     # After 2.5 seconds, apply the brakes. This will only happen once,
     # because on the next iteration the motor will no longer be running.
     if motor_armed and time.perf_counter() > t_brake:
         motor_rotor.idle()
         motor_armed = False
+
 
 # The brake is still applied at this point (try to turn the motor manually),
 # consuming a lot of power, so now we will really turn it off
