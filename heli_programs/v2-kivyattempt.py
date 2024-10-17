c4a4bb998bc3311550377bdf99334e3e5989dd59
tongclement <clement.yc.tong@gmail.com>
2024-10-17 08:51:09 +0100

    Working motor speed and light sensor graphs! Ready for PID implementation

--- heli_programs/v2-kivyattempt.py
+++ heli_programs/v2-kivyattempt.py
@@ -15,6 +15,8 @@ from kivy.properties import NumericProperty
 import matplotlib.pyplot as plt  # If you still want to use matplotlib
 from cued_ia_lego import *  # Assuming this is your proprietary library
 
+#from heli_programs.calibrate_lift import light_readings
+
 # Constants
 SMOOTHING_FACTOR = 0.05
 DATA_DURATION = 60  # seconds to run the data collection
@@ -46,10 +48,14 @@ class MotorController(BoxLayout):
         self.motor_rotor.run()
         self.motor_armed = True
 
+        # Light sensor connected to sensor port 1, LED active
+        self.light = Light(self.brick, PORT_1, illuminated=True)
+
         # Data storage
-        self.times = deque(maxlen=1000)  # Store up to 1000 data points
-        self.position_readings = deque(maxlen=1000)
-        self.speed_readings = deque(maxlen=1000)
+        self.times = deque(maxlen=2000)  # Store up to 2000 data points
+        self.position_readings = deque(maxlen=2000)
+        self.speed_readings = deque(maxlen=2000)
+        self.light_readings=deque(maxlen=2000)
 
         # Set up Graph
         self.graph = Graph(
@@ -59,10 +65,24 @@ class MotorController(BoxLayout):
             padding=5, x_grid=True, y_grid=True, xmin=0, xmax=60, ymin=0, ymax=800
         )
 
+
         self.plot = MeshLinePlot(color=[1, 0, 0, 1])
         self.graph.add_plot(self.plot)
 
         self.add_widget(self.graph)
+
+        # Set up Graph 2 - Light sensor readings
+        self.graphLight = Graph(
+            xlabel='Time (s)', ylabel='Light Sensor Readings',
+            x_ticks_major=10, y_ticks_major=10,
+            y_grid_label=True, x_grid_label=True,
+            padding=5, x_grid=True, y_grid=True, xmin=0, xmax=60, ymin=0, ymax=800
+        )
+
+        self.plotLight = MeshLinePlot(color=[0, 1,0,1])
+        self.graphLight.add_plot(self.plotLight)    #to add to original graph, just do self.graph.add_lot(self.plotDIFFERENTNAME)
+
+        self.add_widget(self.graphLight)
 
         # Set up Buttons
         button_layout = BoxLayout(size_hint_y=0.2)
@@ -96,7 +116,7 @@ class MotorController(BoxLayout):
         # Schedule Graph Update
         Clock.schedule_interval(self.update_graph, 1.0 / 30.0)  # 30 FPS
 
-    def collect_data(self):
+    def collect_data(self): #async running
         t_start = self.start_time
         while self.running and (time.perf_counter() - t_start) < DATA_DURATION:
             current_time = time.perf_counter() - t_start
@@ -125,6 +145,17 @@ class MotorController(BoxLayout):
 
             self.speed_readings.append(smoothed_speed)
 
+            # Calculate one sided moving average of light reading and append, using the smoothing factor logic
+            current_light_reading = self.light.get_lightness()
+            if len(self.light_readings) > 1:
+                #d_time = self.times[-1] - self.times[-2]
+                self.light_readings.append(self.light_readings[-1]*(1- SMOOTHING_FACTOR) + current_light_reading*SMOOTHING_FACTOR)
+
+
+            else:
+                self.light_readings.append(current_light_reading)
+
+
             # Update target speed if needed
             # For example, you can implement pitch adjustments here
 
@@ -140,6 +171,11 @@ class MotorController(BoxLayout):
             self.graph.xmin = max(0, current_time - 60)
 
             self.plot.points = list(zip(self.times, self.speed_readings))
+
+            self.plotLight.points = list(zip(self.times, self.light_readings))
+
+
+
 
     def increase_speed(self, instance):
         self.target_power += 10
